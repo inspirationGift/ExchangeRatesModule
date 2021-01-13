d84c5b093c7435808a5e27c64f9d3082a68752f5
Mark <pivonosovm@gmail.com>
2021-01-13 23:23:55 +0200

    swagger config

--- src/main/java/com/exchange/rate/exchrate/Main.java
+++ src/main/java/com/exchange/rate/exchrate/Main.java
@@ -3,17 +3,25 @@ package com.exchange.rate.exchrate;
 import com.exchange.rate.exchrate.impl.privatBankAPI.PbByDate;
 import org.springframework.web.client.RestTemplate;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        RestTemplate restTemplate = new RestTemplate();
+//        RestTemplate restTemplate = new RestTemplate();
+//
+//        PbByDate arr = restTemplate.getForObject(
+////                "https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5",
+//                "https://api.privatbank.ua/p24api/exchange_rates?json&date=03.01.2021",
+//                PbByDate.class);
+//
+//
+//
+//
+//        System.out.println(Arrays.toString(new PbByDate[]{arr}));
 
-        PbByDate arr = restTemplate.getForObject(
-//                "https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5",
-                "https://api.privatbank.ua/p24api/exchange_rates?json&date=03.01.2021",
-                PbByDate.class);
-
-        System.out.println(Arrays.toString(arr.getExchangeRate()));
+        LocalDate getDate = LocalDate.parse("03.01.2021", DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        System.out.println(getDate.getMonth());
     }
 }
